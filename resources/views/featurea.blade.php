9a67bd43620a1a368e28ac50200786fe701628f4
Shakaet <abdshakaet@gmail.com>
2023-11-25 12:50:50 +0600

    add

--- resources/views/featurea.blade.php
+++ resources/views/featurea.blade.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        a{
+            text-decoration:none;
+            font-size:25px;
+        }
+    </style>
 </head>
 <body>
     <h1>Feature A</h1>
 
-    <a href="{{route('skt')}}">Skt Pages</a>
-    <a href="{{route('featurea')}}">Feature a Pages</a>
-    <a href="{{route('practice')}}">Practice Pages</a>
-    <a href="{{route('welcome')}}">Welcome Pages</a>
+    <a href="{{route('skt')}}">Skt Pages</a><br>
+    <a href="{{route('featurea')}}">Feature a Pages</a><br>
+    <a href="{{route('practice')}}">Practice Pages</a><br>
+    <a href="{{route('welcome')}}">Welcome Pages</a><br>
 
 </body>
 </html>
